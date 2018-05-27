ad5c1fb33045eea1131a9e8a0add219f304ada9b
ice <warpblade@foxmail.com>
2018-05-27 21:29:05 +0800

    v0.3 html和js分离，添加打开不带前缀网址的功能

---
+++ index.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    var objta = document.getElementById("ta");
+    var objOpen = document.getElementById("openbtn");
+
+    objta.innerHTML = 'http://acfun.cn\nhttp://bilibili.com'
+
+    objOpen.addEventListener('click', function () {
+        var urls = objta.value.split("\n")
+
+        for (var url of urls) {
+            //判断当前行是否是一段网址,是否有http等前缀
+            if (/^((https|http|ftp)?:\/\/)[\S]+/.test(url)) {
+                window.open(url);
+                // console.log('http prefix')
+            } else if (/.+\..+\w$/.test(url)) {
+                window.open(`http://${url}`)
+                // console.log('no http prefix')
+            } else {
+                console.log(`not url: ${url}`)
+            }
+        }
+    })
+}
